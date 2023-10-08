c3059305ecf5dbe9a62b40c7ccc6fc49bbcad7c3
SauceBoss1 <dtvsworld@gmail.com>
2023-10-08 00:33:57 -0700

    started the main file

---
+++ QCGauntlet.py
@@ -0,0 +1,26 @@
+import os, sys, shutil, subprocess
+import pandas as pd
+import numpy as np
+
+from modules import *
+
+
+class CommandLine:
+    def __init__(self):
+        import argparse
+
+        self.parser = argparse.ArgumentParser(
+            description="A CLI program that processes datasets and analyzes the quality of it.",
+            prog="QCGauntlet.py",
+            usage="python %(prog)s [command] [arg] [subparser] ...",
+            add_help=True,
+            prefix_chars="-",
+        )
+
+
+def main(inOpts=None):
+    return
+
+
+if __name__ == "__main__":
+    main()
