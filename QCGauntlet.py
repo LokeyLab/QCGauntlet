12f5e9885782ef4909dab91663a6798ff284559d
SauceBoss1 <dtvsworld@gmail.com>
2023-10-08 20:39:15 -0700

    implemented the cp activity score plots

--- QCGauntlet.py
+++ QCGauntlet.py
@@ -3,13 +3,14 @@ import pandas as pd
 import numpy as np
 
 from modules import *
+import modules.cpActivityScoresV2 as cpa
 
 
 class CommandLine:
-    def __init__(self):
+    def __init__(self, inOpts=None):
         import argparse
 
-        # init parser
+        ### init parser ###
         self.parser = argparse.ArgumentParser(
             description="A CLI program that processes datasets and analyzes the quality of it.",
             prog="QCGauntlet.py",
@@ -18,11 +19,232 @@ class CommandLine:
             prefix_chars="-",
         )
 
-        # add subparser
-        self.parser.add_subparsers(title="Gauntlet Analysis", dest="subcommands")
+        #### add subparser ###
+        self.subparser = self.parser.add_subparsers(
+            title="Gauntlet Analysis", dest="subcommands"
+        )
+
+        self.parser.add_argument(
+            "-c",
+            "--mainCond",
+            action="store",
+            nargs="?",
+            type=str,
+            required=True,
+            help="File location/path for the main condition or first condition of the dataset (must be a .csv file)",
+        )
+        self.parser.add_argument(
+            "-ac",
+            "--alternateCond",
+            action="store",
+            nargs="?",
+            type=str,
+            required=False,
+            default=None,
+            help="(optional) File location/path ofr the second/alternate condition of the dataset (must be a .csv file if used)",
+        )
+        self.parser.add_argument(
+            "-o",
+            "--output",
+            nargs="?",
+            action="store",
+            required=True,
+            type=str,
+            help="A string title for the output files (file extensions will be automatically handled)",
+        )
+        self.parser.add_argument(
+            "-t",
+            "--threshold",
+            nargs="?",
+            required=False,
+            type=float,
+            default=0.5,
+            help="Sets the threshold value for analysis. (default: 0.5)",
+        )
+        self.parser.add_argument(
+            "-indCol",
+            "--indexColumn",
+            default=[0],
+            nargs="+",
+            required=False,
+            type=int,
+            action="store",
+            help="Specifies what the index column is in all "
+            "the datasets (annotation sheet/plate maps don't count)",
+        )
+        # self.parser.add_argument(
+        #     "-wl",
+        #     "--wellLabel",
+        #     action="store",
+        #     type=str,
+        #     default="Wells",
+        #     help="The index label of -c and -ac which is of the same name "
+        #     "(note: It cannot be "
+        #     "none as pandas has difficulty parsing un-named indexes)",
+        # )
+
+        ### key df ###
+
+        self.parser.add_argument(
+            "-k",
+            "--keyFile",
+            nargs="?",
+            action="store",
+            required=False,
+            default=None,
+            help="Input path/location of annotation "
+            "sheet/plate map (optional)"
+            "//Use this only if you want to rename the wells in the dataset"
+            "\n[Warning: certain analytical tools may require that this parameter is used]",
+        )
+
+        self.parser.add_argument(
+            "-rc",
+            "--renameColumns",
+            nargs=2,
+            type=str,
+            required=False,
+            default=["unambiguous_name", "longname_proper"],
+            help="If the -k parameter is used then it is highly "
+            "recommended that you use this parameter"
+            " which is in the order of column that specifies "
+            "wells and then column of their renamed values",
+        )
+
+        ############ subparser commands ############
+
+        #### cpactivity ####
+        self.cpActivityScoreSubparser = self.subparser.add_parser(
+            "cpactivity",
+            help="Activates cp Activity Score analysis and generates",
+            add_help=True,
+            prefix_chars="-",
+        )
+        self.cpActivityScoreSubparser.add_argument(
+            "-s",
+            "--sep",
+            default="._.",
+            required=False,
+            nargs="?",
+            action="store",
+            help='Determines the seperator/delimiter used to "\
+                "split text into their plates (default: "._.")',
+        )
+        self.cpActivityScoreSubparser.add_argument(
+            "-pi",
+            "--plateLabelIndex",
+            default=-1,
+            required=False,
+            nargs="?",
+            type=int,
+            action="store",
+            help="After seprating the index labels by the sep command,"
+            " what index are the plate labels on? (def: -1)",
+        )
+        self.cpActivityScoreSubparser.add_argument(
+            "-at",
+            "--activityTitles",
+            nargs="+",
+            required=False,
+            action="store",
+            type=str,
+            help="titles of scores calculated in order of"
+            " -c then -ac params (if -ac is used)",
+        )
+        self.cpActivityScoreSubparser.add_argument(
+            "-ct",
+            "--controlTitles",
+            nargs="+",
+            required=False,
+            action="store",
+            type=str,
+            help="List of control titles/labels (i.e. DMSO PMA)",
+        )
+
+        if inOpts is None:
+            self.args = self.parser.parse_args()
+        else:
+            self.args = self.parser.parse_args(inOpts)
 
 
 def main(inOpts=None):
+    cl = CommandLine(inOpts=inOpts)
+    cond1 = pd.read_csv(cl.args.mainCond, sep=",", index_col=cl.args.indexColumn)
+    cond1.index.name = "wells"
+    cond2 = None
+    if cl.args.alternateCond is not None:
+        cond2 = pd.read_csv(
+            cl.args.alternateCond, sep=",", index_col=cl.args.indexColumn
+        )
+        cond2.index.name = "wells"
+
+    key = cl.args.keyFile
+    if key is not None:
+        key = pd.read_csv(key)
+        key = key[cl.args.renameColumns]
+        key.set_index(cl.args.renameColumns[0], inplace=True)
+
+    if cl.args.subcommands == "cpactivity":
+        # properties and config:
+        # sep x
+        # plateLabelIndex x
+        # map -> sep function or subparser
+        # activityTitles x
+        # control Titles x
+        # wellLabels
+        # renameColumn
+        resDataset = cpa.createDataScores(
+            compDf=cond1,
+            noCompDf=cond2,
+            sep=cl.args.sep,
+            plateLabelIndex=cl.args.plateLabelIndex,
+            activityTitles=cl.args.activityTitles,
+            controlTitle=cl.args.controlTitles,
+            # wellLabels=cl.args.wellLabel,
+            wellLabels="wells",
+            renameColumn=cl.args.renameColumns[1],
+            map=key,
+        )
+
+        cpa.analyzeDf(
+            dataset=resDataset,
+            compLabel=cl.args.activityTitles[0],
+            threshold=cl.args.threshold,
+            noCompLabel=cl.args.activityTitles[1] if cond2 is not None else None,
+            outName=f"{cl.args.output}_activityScore.xlsx",
+        )
+        if cond2 is None:
+            cpa.genIndivElbows(
+                ds=resDataset,
+                groupByCol="plate",
+                yCol=cl.args.activityTitles[0],
+                outname=f"{cl.args.output}_elboPlot.pdf",
+                threshold=cl.args.threshold,
+                control=cl.args.controlTitles,
+                sep=cl.args.sep,
+            )
+        else:
+            g = cpa.createMultiPlot(
+                ds=resDataset,
+                groupByCol="plate",
+                x=cl.args.activityTitles[0],
+                y=cl.args.activityTitles[1],
+                threshold=cl.args.threshold,
+                hue="well_type",
+                controlTitles=cl.args.controlTitles,
+            )
+            g.savefig(f"{cl.args.output}_multiPlot.pdf", format="pdf", dpi=320)
+
+            cpa.genIndviPlots(
+                ds=resDataset,
+                groupByCol="plate",
+                xCol=cl.args.activityTitles[0],
+                yCol=cl.args.activityTitles[1],
+                outname=f"{cl.args.output}_individualPlots.pdf",
+                threshold=cl.args.threshold,
+                control=cl.args.controlTitles,
+            )
+
     return
 
 
--- modules/cpActivityScoresV2.py
+++ modules/cpActivityScoresV2.py
@@ -3,166 +3,271 @@ import os, subprocess, shutil, sys
 from matplotlib.backends.backend_pdf import PdfPages
 from modules import *
 
-def getPlateActivityScores(compoundDf: pd.DataFrame,\
-                        noCompDf: pd.DataFrame = None,\
-                        map = None,\
-                        activityTitles = ['pma_ActivityScores', 'noPma_ActivtyScores'],\
-                        controlTitle = ['DMSO', 'PMA'],\
-                        sep='._.', wellLabels = 'Wells',\
-                        renameColumn = 'longname_proper'):
-    wells = [str(i).split('._.')[0] for i in list(compoundDf.index.get_level_values(wellLabels))]
+
+def getPlateActivityScores(
+    compoundDf: pd.DataFrame,
+    noCompDf: pd.DataFrame = None,
+    map=None,
+    activityTitles=["pma_ActivityScores", "noPma_ActivtyScores"],
+    controlTitle=["DMSO", "PMA"],
+    sep="._.",
+    wellLabels="Wells",
+    renameColumn="longname_proper",
+    **kwargs,
+):
+    wells = [
+        str(i).split("._.")[0]
+        for i in list(compoundDf.index.get_level_values(wellLabels))
+    ]
 
     try:
-        plateName = list(noCompDf.index.get_level_values('plates'))
+        plateName = list(noCompDf.index.get_level_values("plates"))
     except AttributeError:
-        plateName = list(compoundDf.index.get_level_values('plates'))
+        plateName = list(compoundDf.index.get_level_values("plates"))
 
     if noCompDf is None:
         plateComp = calculateScore(df=compoundDf.copy())
         rawData = {
-            activityTitles[0]:plateComp.to_numpy(),
-            'plate': plateName,
-            'wells': wells
+            activityTitles[0]: plateComp.to_numpy(),
+            "plate": plateName,
+            "wells": wells,
         }
     else:
         plateComp = calculateScore(df=compoundDf.copy())
         plateNoCompDf = calculateScore(df=noCompDf.copy())
         rawData = {
-            activityTitles[0]:plateComp.to_numpy(),
-            activityTitles[1]:plateNoCompDf.to_numpy(),
-            'plate': plateName,
-            'wells': wells
+            activityTitles[0]: plateComp.to_numpy(),
+            activityTitles[1]: plateNoCompDf.to_numpy(),
+            "plate": plateName,
+            "wells": wells,
         }
-    
+
     acScoreDf = pd.DataFrame(rawData)
     acScoreDf.sort_index(inplace=True)
 
     if map is not None:
         namingDf = compoundDf if noCompDf is None else noCompDf
-        renamedDf = renameKeys(mainDf=namingDf, keyDf=map, renameColumn=renameColumn, left_on=wellLabels)
-        acScoreDf['Full Proper Name'] = list(renamedDf.index)
-        acScoreDf.set_index('Full Proper Name', inplace=True)
+        renamedDf = renameKeys(
+            mainDf=namingDf, keyDf=map, renameColumn=renameColumn, left_on=wellLabels
+        )
+        acScoreDf["Full Proper Name"] = list(renamedDf.index)
+        acScoreDf.set_index("Full Proper Name", inplace=True)
     else:
-        acScoreDf['Full Proper Name'] = list(compoundDf.index.get_level_values(wellLabels)) if noCompDf is None else list(noCompDf.index.get_level_values(wellLabels))
-        acScoreDf.set_index('Full Proper Name', inplace=True)
-    inclSep = lambda x: ''.join([x,sep])
-    wellType = [f'CONTROL_{controlTitle[0]}' if inclSep(controlTitle[0]) in i else f'CONTROL_{controlTitle[1]}' if inclSep(controlTitle[1]) in i else 'EXPERIMENTAL' for i in list(acScoreDf.index)]
-    acScoreDf['well_type'] = wellType
+        acScoreDf["Full Proper Name"] = (
+            list(compoundDf.index.get_level_values(wellLabels))
+            if noCompDf is None
+            else list(noCompDf.index.get_level_values(wellLabels))
+        )
+        acScoreDf.set_index("Full Proper Name", inplace=True)
+    inclSep = lambda x: "".join([x, sep])
+    wellType = [
+        f"CONTROL_{controlTitle[0]}"
+        if inclSep(controlTitle[0]) in i
+        else f"CONTROL_{controlTitle[1]}"
+        if inclSep(controlTitle[1]) in i
+        else "EXPERIMENTAL"
+        for i in list(acScoreDf.index)
+    ]
+    acScoreDf["well_type"] = wellType
 
     return acScoreDf
 
+
 def createDataScores(compDf: pd.DataFrame, noCompDf: pd.DataFrame = None, **kwargs):
     acScores = []
-    sep = kwargs.get('sep','._.')
+    sep = kwargs.get("sep", "._.")
     compDf = parsePlates(inDf=compDf, **kwargs)
 
     if noCompDf is not None:
         noCompDf = parsePlates(inDf=noCompDf, **kwargs)
-        for (compName, currCompDf), (noCompName, currNoCompDf) in zip(compDf.groupby(level='plates'), noCompDf.groupby(level='plates')):
-            acScores.append(getPlateActivityScores(compoundDf=currCompDf, noCompDf=currNoCompDf, **kwargs))
+        for (compName, currCompDf), (noCompName, currNoCompDf) in zip(
+            compDf.groupby(level="plates"), noCompDf.groupby(level="plates")
+        ):
+            acScores.append(
+                getPlateActivityScores(
+                    compoundDf=currCompDf, noCompDf=currNoCompDf, **kwargs
+                )
+            )
     else:
-        for compName, currCompDf in compDf.groupby(level='plates'):
-            acScores.append(getPlateActivityScores(compoundDf=currCompDf, noCompDf=noCompDf, **kwargs))
+        for compName, currCompDf in compDf.groupby(level="plates"):
+            acScores.append(
+                getPlateActivityScores(
+                    compoundDf=currCompDf, noCompDf=noCompDf, **kwargs
+                )
+            )
     return pd.concat(acScores, axis=0)
 
-def createMultiPlot(ds, groupByCol, x, y, col_wrap=5, snsPlot=sns.scatterplot, threshold=0.5, hue=None, controlTitles = ['DMSO', 'PMA']):
+
+def createMultiPlot(
+    ds,
+    groupByCol,
+    x,
+    y,
+    col_wrap=5,
+    snsPlot=sns.scatterplot,
+    threshold=0.5,
+    hue=None,
+    controlTitles=["DMSO", "PMA"],
+):
     palette = None
     if hue is not None:
-        palette = {f'CONTROL_{controlTitles[0]}': 'purple', f'CONTROL_{controlTitles[1]}':'limegreen','EXPERIMENTAL': 'C0'}
+        palette = {
+            f"CONTROL_{controlTitles[0]}": "purple",
+            f"CONTROL_{controlTitles[1]}": "limegreen",
+            "EXPERIMENTAL": "C0",
+        }
 
     g = sns.FacetGrid(ds, col=groupByCol, col_wrap=col_wrap, hue=hue, palette=palette)
     g.map_dataframe(func=snsPlot, x=x, y=y, alpha=0.75)
 
     for ax in g.axes.flat:
-        ax.axvline(x=threshold, color='r', zorder=2)
-        ax.axhline(y=threshold, color='r', zorder=2)
+        ax.axvline(x=threshold, color="r", zorder=2)
+        ax.axhline(y=threshold, color="r", zorder=2)
     return g
 
-def genIndviPlots(ds: pd.DataFrame, groupByCol, xCol, yCol, outname,threshold=0.5, control=['DMSO', 'PMA']):
+
+def genIndviPlots(
+    ds: pd.DataFrame,
+    groupByCol,
+    xCol,
+    yCol,
+    outname,
+    threshold=0.5,
+    control=["DMSO", "PMA"],
+):
     groups = ds.groupby(groupByCol)
 
     with PdfPages(outname) as p:
-        for name, df, in groups:
-            fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(3*4, 4)) # make subplots of 1 row, 3 cols and then the next 2 cols are the elbow plots (sorted CP score)
+        for (
+            name,
+            df,
+        ) in groups:
+            fig, ax = plt.subplots(
+                nrows=1, ncols=3, figsize=(3 * 4, 4)
+            )  # make subplots of 1 row, 3 cols and then the next 2 cols are the elbow plots (sorted CP score)
 
-            colors = ['purple' if i== f'CONTROL_{control[0]}' else 'limegreen' if  i== f'CONTROL_{control[1]}' else 'C0' for i in df['well_type'].to_list()]
-            df.plot.scatter(x=xCol, y = yCol, ax=ax[0], title = f'plate: {name} scatter plot', c=colors, alpha=0.75)
-            ax[0].axvline(x=threshold, color='r', zorder=2)
-            ax[0].axhline(y=threshold, color='r', zorder=2)
+            colors = [
+                "purple"
+                if i == f"CONTROL_{control[0]}"
+                else "limegreen"
+                if i == f"CONTROL_{control[1]}"
+                else "C0"
+                for i in df["well_type"].to_list()
+            ]
+            df.plot.scatter(
+                x=xCol,
+                y=yCol,
+                ax=ax[0],
+                title=f"plate: {name} scatter plot",
+                c=colors,
+                alpha=0.75,
+            )
+            ax[0].axvline(x=threshold, color="r", zorder=2)
+            ax[0].axhline(y=threshold, color="r", zorder=2)
 
             if control is not None:
-                newDf = df[~(df.index.str.contains('|'.join(control)))]
+                newDf = df[~(df.index.str.contains("|".join(control)))]
             else:
                 newDf = df
-            
+
             compScore = newDf[xCol].sort_values().values
             noCompScore = newDf[yCol].sort_values().values
 
             x = [i for i in range(len(compScore))]
 
-            ax[1].plot(x, compScore, linestyle='-', color='blue')
-            ax[1].axhline(y=threshold, color='r', zorder=2)
-            ax[1].set_xlabel('Compounds')
+            ax[1].plot(x, compScore, linestyle="-", color="blue")
+            ax[1].axhline(y=threshold, color="r", zorder=2)
+            ax[1].set_xlabel("Compounds")
             ax[1].set_ylabel(xCol)
-            ax[1].set_title(f'plate: {name} elbow plot')
+            ax[1].set_title(f"plate: {name} elbow plot")
 
-            ax[2].plot(x, noCompScore, linestyle='-', color='blue')
-            ax[2].axhline(y=threshold, color='r', zorder=2)
-            ax[2].set_xlabel('Compounds')
+            ax[2].plot(x, noCompScore, linestyle="-", color="blue")
+            ax[2].axhline(y=threshold, color="r", zorder=2)
+            ax[2].set_xlabel("Compounds")
             ax[2].set_ylabel(yCol)
-            ax[2].set_title(f'plate: {name} elbow plot')
-
+            ax[2].set_title(f"plate: {name} elbow plot")
 
             fig.tight_layout()
-            fig.savefig(p, format='pdf', dpi=320)
+            fig.savefig(p, format="pdf", dpi=320)
             plt.close(fig=fig)
 
-def genIndivElbows(ds: pd.DataFrame, groupByCol, yCol, outname, threshold=0.5, control=['DMSO','PMA'], sep='._.'):
+
+def genIndivElbows(
+    ds: pd.DataFrame,
+    groupByCol,
+    yCol,
+    outname,
+    threshold=0.5,
+    control=["DMSO", "PMA"],
+    sep="._.",
+):
     groups = ds.groupby(groupByCol)
 
     with PdfPages(outname) as pdf:
         for name, df in groups:
-            fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(4,4))
+            fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(4, 4))
 
-            controlNames = [f'{control[i]}{sep}' for i in range(len(control))]
+            controlNames = [f"{control[i]}{sep}" for i in range(len(control))]
             newDf = df
             if control is not None:
-                newDf = df[~(df.index.str.contains('|'.join(controlNames)))]
-            
+                newDf = df[~(df.index.str.contains("|".join(controlNames)))]
+
             scores = newDf[yCol].sort_values().values
             x = [i for i in range(len(scores))]
 
-            ax.plot(x, scores, linestyle='-', color='blue')
-            ax.axhline(y=threshold, color='r', zorder=2)
-            ax.set_xlabel('Compounds')
+            ax.plot(x, scores, linestyle="-", color="blue")
+            ax.axhline(y=threshold, color="r", zorder=2)
+            ax.set_xlabel("Compounds")
             ax.set_ylabel(yCol)
-            ax.set_title(f'plate: {name} elbow plot')
+            ax.set_title(f"plate: {name} elbow plot")
 
             fig.tight_layout()
             pdf.savefig(figure=fig, dpi=320)
             plt.close(fig=fig)
 
-def analyzeDf(dataset: pd.DataFrame, compLabel, outName, threshold=0.5, noCompLabel=None):
-    
+
+def analyzeDf(
+    dataset: pd.DataFrame, compLabel, outName, threshold=0.5, noCompLabel=None
+):
     fullActivityScores = dataset.copy()
-    with pd.ExcelWriter(outName, engine='xlsxwriter') as writer:
-        fullActivityScores.to_excel(writer, sheet_name='All ActivityScores')
+    with pd.ExcelWriter(outName, engine="xlsxwriter") as writer:
+        fullActivityScores.to_excel(writer, sheet_name="All ActivityScores")
 
         if noCompLabel is not None:
-            noActivityInBoth = fullActivityScores[(fullActivityScores[compLabel] < threshold) & (fullActivityScores[noCompLabel] < threshold)].copy()
-            noActivityInBoth.to_excel(writer, sheet_name= f'PMA and noPMA (both < {threshold})')
+            noActivityInBoth = fullActivityScores[
+                (fullActivityScores[compLabel] < threshold)
+                & (fullActivityScores[noCompLabel] < threshold)
+            ].copy()
+            noActivityInBoth.to_excel(
+                writer, sheet_name=f"PMA and noPMA (both < {threshold})"
+            )
 
-            activityInBoth = fullActivityScores[(fullActivityScores[compLabel] >= threshold) & (fullActivityScores[noCompLabel] >= threshold)].copy()
-            activityInBoth.to_excel(writer, sheet_name= f'PMA and noPMA (both >= {threshold})')
+            activityInBoth = fullActivityScores[
+                (fullActivityScores[compLabel] >= threshold)
+                & (fullActivityScores[noCompLabel] >= threshold)
+            ].copy()
+            activityInBoth.to_excel(
+                writer, sheet_name=f"PMA and noPMA (both >= {threshold})"
+            )
 
-            activityInCompoundsVsnoCompounds = fullActivityScores[(fullActivityScores[compLabel] >= threshold) & (fullActivityScores[noCompLabel] < threshold)].copy()
-            activityInCompoundsVsnoCompounds.to_excel(writer,sheet_name=f'PMA>={threshold}; noPMA<{threshold}')
+            activityInCompoundsVsnoCompounds = fullActivityScores[
+                (fullActivityScores[compLabel] >= threshold)
+                & (fullActivityScores[noCompLabel] < threshold)
+            ].copy()
+            activityInCompoundsVsnoCompounds.to_excel(
+                writer, sheet_name=f"PMA>={threshold}; noPMA<{threshold}"
+            )
 
-            activityInNoCompoundsVsCompounds = fullActivityScores[(fullActivityScores[compLabel] < threshold) & (fullActivityScores[noCompLabel] >= threshold)].copy()
-            activityInNoCompoundsVsCompounds.to_excel(writer, sheet_name=f'PMA<{threshold}; noPMA>={threshold}')
+            activityInNoCompoundsVsCompounds = fullActivityScores[
+                (fullActivityScores[compLabel] < threshold)
+                & (fullActivityScores[noCompLabel] >= threshold)
+            ].copy()
+            activityInNoCompoundsVsCompounds.to_excel(
+                writer, sheet_name=f"PMA<{threshold}; noPMA>={threshold}"
+            )
         else:
             active = fullActivityScores[(fullActivityScores[compLabel] >= threshold)]
-            active.to_excel(writer, sheet_name=f'scores >= {threshold}')
+            active.to_excel(writer, sheet_name=f"scores >= {threshold}")
 
             notActive = fullActivityScores[(fullActivityScores[compLabel] < threshold)]
-            notActive.to_excel(writer, sheet_name=f'scores < {threshold}')
+            notActive.to_excel(writer, sheet_name=f"scores < {threshold}")
