85762d15513a708d01aeb18f5315e00af2c65ce5
SauceBoss1 <dtvsworld@gmail.com>
2023-10-16 14:14:40 -0700

    added windows line

--- package.sh
+++ package.sh
@@ -1,1 +1,6 @@
-pyinstaller --onefile --windowed --clean --name=QCGauntlet --target-architecture arm64 --icon=icons/calculator_data_analysis_icon.ico QCGauntletApp.py
+# MacO
+#pyinstaller --onefile --windowed --clean --name=QCGauntlet --target-architecture arm64 --icon=icons/calculator_data_analysis_icon.ico QCGauntletApp.py
+
+
+# Windows
+pyinstaller --onefile --windowed --clean --name=QCGauntlet --icon=icons/calculator_data_analysis_icon.ico QCGauntletApp.py
